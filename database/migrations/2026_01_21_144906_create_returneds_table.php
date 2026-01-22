42d66da1ada91ce59e404890217ec9f11a19e336
jkian <slyrics443@gmail.com>
2026-01-22 23:20:14 +0800

    added columns to ingredient

--- database/migrations/2026_01_21_144906_create_returneds_table.php
+++ database/migrations/2026_01_21_144906_create_returneds_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('returneds', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained();
+            $table->foreignId('branch_id')->constrained();
             $table->timestamps();
         });
     }
--- database/migrations/2026_01_21_144957_create_ingredients_table.php
+++ database/migrations/2026_01_21_144957_create_ingredients_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('threshold');
+            $table->string('measurement');
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
         });
     }

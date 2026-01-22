ed1220da3bb702a2ddfe25229f3a577d7f98bf85
jkian <slyrics443@gmail.com>
2026-01-22 23:25:13 +0800

    made new miration product_return pivot

---
+++ database/migrations/2026_01_22_152439_create_product_return_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_return', function (Blueprint $table) {
+         $table->foreignId('product_id')->constrained();
+            $table->foreignId('return_id')->constrained();
+            $table->decimal('price',8,2)->unsigned();
+            $table->decimal('quantity',8,2)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_return');
+    }
+};

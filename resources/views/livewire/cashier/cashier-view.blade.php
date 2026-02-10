0e7752c3098af140f37747d52590798f0349e754
jkian <slyrics443@gmail.com>
2026-02-11 00:33:08 +0800

    finish: layout of two container

--- resources/views/livewire/cashier/cashier-view.blade.php
+++ resources/views/livewire/cashier/cashier-view.blade.php
@@ -1,9 +1,47 @@
-<div >
+<div class="cashier-container">
     <!-- class="cashier-container" -->
 
     <div class="product-container">
 
         <div class="card product-card">
+
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+         <div class="card product-card">
+
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+        <div class="card product-card">
+            <h1>asdjfalds</h1>
+        </div>
+         <div class="card product-card">
 
             <h1>asdjfalds</h1>
         </div>

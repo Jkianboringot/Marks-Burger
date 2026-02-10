1f3aa780181e55eedd05127b2c308508aac39d1f
jkian <slyrics443@gmail.com>
2026-02-10 20:04:21 +0800

    adding file for main cashier

---
+++ app/Livewire/Cashier/CashierView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Cashier;
+
+use Livewire\Component;
+
+class CashierView extends Component
+{
+    public function render()
+    {
+        return view('livewire.cashier.cashier-view');
+    }
+}

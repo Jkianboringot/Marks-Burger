ff8924b8b6ca3db9d43b298d738bf059ecc93d3c
jkian <slyrics443@gmail.com>
2026-02-11 01:37:10 +0800

    fix box content

--- resources/views/livewire/cashier/cashier-view.blade.php
+++ resources/views/livewire/cashier/cashier-view.blade.php
@@ -3,92 +3,86 @@
 
     <div class="product-container">
 
-        <div class="card product-card">
+        <div class="card product-box">
 
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-         <div class="card product-card">
+         <div class="card product-box">
 
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-         <div class="card product-card">
+         <div class="card product-box">
 
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card product-box">
             <h1>asdjfalds</h1>
         </div>
     </div>
     <div class="selected-container" >
 
-        <div class="card product-card">
+        <div class="card selection-box">
 
             <h1>asd</h1>
         </div>
-        <div class="card product-card">
+        <div class="card selection-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card selection-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card selection-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card selection-box">
             <h1>asdjfalds</h1>
         </div>
-        <div class="card product-card">
+        <div class="card selection-box">
             <h1>asdjfalds</h1>
         </div>
     </div>
-    <!-- <div class="selected-container">
 
-        <div class="card product-card">
-
-            <h1>asdjfalds</h1>
-        </div>
-    </div> -->
 
 </div>

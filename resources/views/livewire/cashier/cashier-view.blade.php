a6aed427f7072d4c956da103dcc4435a9e86c8be
jkian <slyrics443@gmail.com>
2026-02-11 21:48:29 +0800

    added: inner selection

--- resources/views/livewire/cashier/cashier-view.blade.php
+++ resources/views/livewire/cashier/cashier-view.blade.php
@@ -2,92 +2,144 @@
     <!-- class="cashier-container" -->
 
     <div class="total-container">
-         <h1>Total</h1>
+        <h1>Total</h1>
     </div>
 
 
     <div class="product-container">
 
         <div class="card product-box">
-
-            <h1>asdjfalds</h1>
-        </div>
-        <div class="card product-box">
-            <div class="inner">
-                <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image">
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
             </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
-         <div class="card product-box">
+        <div class="card product-box">
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
+        </div>
+        <div class="card product-box">
 
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
-         <div class="card product-box">
+        <div class="card product-box">
 
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card product-box">
-            <h1>asdjfalds</h1>
+            <div class="inner-product">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
     </div>
-    <div class="selected-container" >
+
+
+    <div class="selected-container">
 
         <div class="card selection-box">
 
-            <h1>asd</h1>
+            <div class="inner-selection">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card selection-box">
-            <h1>asdjfalds</h1>
+
+            <div class="inner-selection">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card selection-box">
-            <h1>asdjfalds</h1>
+
+            <div class="inner-selection">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card selection-box">
-            <h1>asdjfalds</h1>
+
+            <div class="inner-selection">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card selection-box">
-            <h1>asdjfalds</h1>
+
+            <div class="inner-selection">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
         <div class="card selection-box">
-            <h1>asdjfalds</h1>
+
+            <div class="inner-selection">
+                <!-- <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image"> -->
+            </div>
         </div>
     </div>
 

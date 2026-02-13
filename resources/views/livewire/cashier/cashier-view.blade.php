97d8e7f1390165a0de18e827dfd7333c2c586075
jkian <slyrics443@gmail.com>
2026-02-13 22:27:46 +0800

    put all varaible name and fix a little things in cahsier view

--- resources/views/livewire/cashier/cashier-view.blade.php
+++ resources/views/livewire/cashier/cashier-view.blade.php
@@ -2,29 +2,41 @@
     <!-- class="cashier-container" -->
 
     <div class="total-container">
+    
 
+<!-- 
+    i really need to improve this , its just devs everywhere hahaha, its the only thing i 
+    kow how to work without ai , bcause am pretty sure you dont just spam devs here-->
         <div>
             <h1>Total
             </h1>
         </div>
         <div class="total">
-            <h3 >89</h3>
+            <h3 >total_variable</h3>
+                
         </div>
     </div>
 
 
     <div class="product-container">
 
+
+    <!-- when this is only one product it just puts it in the center i dont want that, fix it later -->
         <div class="card product-box">
             <div class="inner-product">
 
                 <img src="admin-lte\assets\img\ego-profile.jpg" alt="No Image">
-                <h1 class="inner-product-text">Burger</h1>
+                <h1 class="inner-product-text">product_name</h1>
 
             </div>
             <div class="product-description">
-                <h2 class="product-price text-title">Price: <span class="text-variable">89</span></h2>
-                <h2 class="product-quantity text-title">Quantity: <span class="text-variable">89</span></h2>
+                <h2 class="product-price text-title">Price: <span class="text-variable">
+                    <!-- price_variable -->
+
+                </span></h2>
+                <h2 class="product-quantity text-title">Quantity: <span class="text-variable">
+                    <!-- current_quantity_variable -->
+                </span></h2>
             </div>
         </div>
         <div class="card product-box">
@@ -196,10 +208,17 @@
 
             </div>
             <div class="selection-description">
-                <h2 class="text-title">Name<span class="text-variable">89</span></h2>
+                <h2 class="text-title">Name:<span class="text-variable">89
+                    <!-- product_name
+                     really need this shit to be eager loaded and hydrated what ever that means -->
+            </span></h2>
                 <!-- <h2 class="text-title">Price<span class="text-variable">89</span></h2> -->
-                <h2 class="text-title">Quantity<span class="text-variable">89</span></h2>
-                <h2 class="text-title">total<span class="text-variable">89</span></h2>
+                <h2 class="text-title">Quantity:<span class="text-variable">89
+                    <!-- total_quantity_variable -->
+                </span></h2>
+                <h2 class="text-title">total:<span class="text-variable">89
+                    <!-- total_per_product  _variable -->
+                </span></h2>
             </div>
         </div>
         <div class="card selection-box">
